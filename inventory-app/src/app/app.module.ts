70dd363c07cb1da8f02bb44ee64b9f7da3efab68
stevenchen <qwe16165850@gmail.com>
2017-09-21 22:22:13 +1000

    finish app-products-list

--- inventory-app/src/app/app.module.ts
+++ inventory-app/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { PriceDisplayComponent } from './price-display/price-display.component';
@@ -18,7 +20,9 @@ import { ProductsListComponent } from './products-list/products-list.component';
     ProductsListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- inventory-app/src/app/products-list/products-list.component.ts
+++ inventory-app/src/app/products-list/products-list.component.ts
@@ -1,19 +1,45 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {Product} from "../product.model";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output
+} from '@angular/core';
+import { Product } from '../product.model';
 
+/**
+ * @ProductsList: A component for rendering all ProductRows and
+ * storing the currently selected Product
+ */
 @Component({
-  selector: 'app-products-list',
-  templateUrl: './products-list.component.html',
-  styleUrls: ['./products-list.component.css']
+  selector: 'products-list',
+  templateUrl: './products-list.component.html'
 })
-export class ProductsListComponent implements OnInit {
+export class ProductsListComponent {
 
   @Input() productList: Product[];
 
   @Output() onProductSelected: EventEmitter<Product>;
-  constructor() { }
 
-  ngOnInit() {
+  /**
+   * @property currentProduct - local state containing
+   *             the currently selected `Product`
+   */
+  private currentProduct: Product;
+
+  constructor() {
+    this.onProductSelected = new EventEmitter();
+  }
+
+  clicked(product: Product): void {
+    this.currentProduct = product;
+    this.onProductSelected.emit(product);
+  }
+
+  isSelected(product: Product): boolean {
+    if (!product || !this.currentProduct) {
+      return false;
+    }
+    return product.sku === this.currentProduct.sku;
   }
 
 }
