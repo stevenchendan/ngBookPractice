ad135a0146eeae9c28eb08e87cf9300b466500d2
stevenchen <qwe16165850@gmail.com>
2017-09-24 22:11:39 +1000

    product row view and controller

--- inventory-app/src/app/product-row/product-row.component.ts
+++ inventory-app/src/app/product-row/product-row.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostBinding, Input, OnInit} from '@angular/core';
+import {Product} from '../product.model';
 
 @Component({
-  selector: 'app-product-row',
+  selector: 'product-row',
   templateUrl: './product-row.component.html',
   styleUrls: ['./product-row.component.css']
 })
 export class ProductRowComponent implements OnInit {
+
+  @Input()
+  product: Product;
+  @HostBinding('attr.class') cssClass = 'item';
 
   constructor() { }
 
