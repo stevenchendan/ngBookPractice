cba84d23b9a9bafa5a3581ca52b28bbe7d6049bf
stevenchen <qwe16165850@gmail.com>
2017-10-03 22:19:35 +1100

    add ng-style-example

--- built-in-directives/src/app/ng-style-example/ng-style-example.component.ts
+++ built-in-directives/src/app/ng-style-example/ng-style-example.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ng-style-example.component.css']
 })
 export class NgStyleExampleComponent implements OnInit {
-
+  color: string;
+  fontSize: number;
   constructor() { }
 
   ngOnInit() {
   }
 
+  apply(color: string, fontSize: number): void {
+    this.color = color;
+    this.fontSize = fontSize;
+  }
 }
