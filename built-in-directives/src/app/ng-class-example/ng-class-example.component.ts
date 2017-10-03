173f8724ba40052aad813b8e22d55d72c140fc7b
stevenchen <qwe16165850@gmail.com>
2017-10-04 00:46:18 +1100

    follow ng-class-example

--- built-in-directives/src/app/ng-class-example/ng-class-example.component.ts
+++ built-in-directives/src/app/ng-class-example/ng-class-example.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ng-class-example.component.css']
 })
 export class NgClassExampleComponent implements OnInit {
+  isBordered: Boolean;
+  classesObj: Object;
+  classList: string[];
 
   constructor() { }
 
   ngOnInit() {
+    this.isBordered = true;
+    this.classList = ['blue', 'round'];
+    this.toggleBorder();
+  }
+
+  toggleBorder(): void {
+    this.isBordered = !this.isBordered;
+    this.classesObj = {
+      bordered: this.isBordered
+    };
+  }
+
+  toggleClass(cssClass: string): void {
+    const pos: number = this.classList.indexOf(cssClass);
+    if (pos > -1) {
+      this.classList.splice(pos, 1);
+    } else {
+      this.classList.push(cssClass);
+    }
   }
 
 }
