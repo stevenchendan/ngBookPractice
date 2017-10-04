68c46599aaed60f861308e57a80052dcde5c4c8e
stevenchen <qwe16165850@gmail.com>
2017-10-04 22:45:59 +1100

    practice ngFor

--- built-in-directives/src/app/ng-for-example/ng-for-example.component.ts
+++ built-in-directives/src/app/ng-for-example/ng-for-example.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ng-for-example.component.css']
 })
 export class NgForExampleComponent implements OnInit {
-
+  cities: string[];
+  people: Object[];
+  peopleByCity: Object;
   constructor() { }
 
   ngOnInit() {
+    this.cities = ['Miami', 'San Paulo', 'New York'];
+    this.people = [
+      { name: 'Anderson', age: 35, city: 'Sao Paulo'},
+      { name: 'Steven', age: 27, city: 'Sydney'},
+      { name: 'Peter', age: 35, city: 'wollongong'},
+    ];
+    this.peopleByCity = [
+      {
+        city: 'Miami',
+        people: [
+          { name: 'Steven', age: 12},
+          { name: 'Angel', age: 13},
+        ]
+      },
+      {
+        city: 'Sao Paulo',
+        people: [
+          { name: 'Anderson', age: 35 },
+          { name: 'Felope', age: 36}
+        ]
+      }
+    ];
   }
 
 }
